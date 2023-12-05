21e2d7e03f44950cb463e52e01187eb16c04a452
willians2022 <willians_zno@hotmail.com>
2023-12-05 11:28:08 -0300

    realização do 3 exercioio

---
+++ desafio 1 calcular a distancia de km/primeiro desafio.js
@@ -0,0 +1,19 @@
+/*faca um programa para calcular o valor de uma viagem calcular
+
+
+voçê tear 3 variveis . sendo elas :
+
+1 - preço do combustivel ;
+2-  gasto medio de combustivel ;
+3- distancia em km da viagem ; */
+
+
+const precoCombustivel = 5.50;
+const kmPorLitro = 10 ;
+const distanciaEmKm = 4000;
+
+const listroConsumidos = distanciaEmKm / kmPorLitro;
+const valorGasto = listroConsumidos * precoCombustivel;
+console.log(valorGasto.toFixed(2));
+
+
