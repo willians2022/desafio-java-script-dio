7978858468e30bcd82495879c0169e879aa7ff2c
willians2022 <willians_zno@hotmail.com>
2023-12-06 19:45:52 -0300

    funcoes

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+function escrevaMeuNome(nome) {
+    return 'meu nome é ' + nome;
+}
+
+
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        console.log(escrevaMeuNome('willians') + ' maior ')
+    } else {
+        ('menor')
+        console.log(escrevaMeuNome('willians') + ' menor');
+    }
+}
+
+verificarIdade(11);
---
+++ funcoes5.js
@@ -0,0 +1,44 @@
+/* O IMC – Indice de Massa Corporal é um critério da Organização Mundial de Saúde para dar uma indicação sobre a condição de peso de uma pessoa adulta. 
+
+Formula do IMC:
+IMC = peso / (altura * altura)
+
+Elabore um algoritmo que dado o peso e a altura de um adulto mostre sua condição de acordo com a tabela abaixo.
+
+IMC em adultos Condição:
+- Abaixo de 18.5 Abaixo do peso;
+- Entre 18.5 ate 25 Peso normal;
+- Entre 25 e 30 Acima do peso;
+- Entre 30 e 40 Obeso;
+- Acima de 40 Obsesidade Grave;
+
+*/
+
+function calcularImc(peso, altura) {
+    return peso / Math.pow(altura, 2);
+}
+
+
+function classificarImc(imc) {
+    if (imc < 18.5) {
+        return 'abaixo do peso ';
+    } else if (imc > 18.5 && imc < 25) {
+        return 'peso normal';
+    } else if (imc > 25 && imc < 30) {
+        return 'acima do peso';
+    } else if (imc > 30 && imc < 40) {
+        return 'obeso';
+    } else {
+        'Obesidade grave';
+    } f
+}
+
+function main() {
+    const peso = 27;
+    const altura = 1.27;
+    const imc = calcularImc(peso, altura);
+    console.log(classificarImc(imc));
+}
+
+
+main();
