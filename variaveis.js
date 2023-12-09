3999586cb7fe9cfe06ac7bc021e03621132d043f
willians2022 <willians_zno@hotmail.com>
2023-12-09 19:10:39 -0300

    variaves e classes

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+
+// Exitem 3 tipo de variaveis  var , let e const .
+
+
+
+var  var1 = 1;   
+var  var2 = "teste";
+console.log (var1 + var2);
+//resultado = 1teste 
+
+const var3 = 3;
+const var4 = 4;
+console.log(var3+var4);
+//resultado = 7
+
+let var5 = 5
+let var6 = 6
+console.log(var5+var6);
+// resultado = 11
+
+
