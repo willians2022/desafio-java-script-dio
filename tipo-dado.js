adaa07aadedbaebd0a1448fbd0c44245c0da0f1f
willians2022 <willians_zno@hotmail.com>
2023-12-11 16:49:33 -0300

    tipos de variaveis

---
+++ tipo-dado.js
@@ -0,0 +1,24 @@
+/*
+tipo primitivos 
+
+boolean
+null
+undefined
+number
+string
+synbol
+
+
+// objeto                           saida 
+
+console.log( 10 + '10');          // 1010
+console.log( 10 - '10');          //0
+console.log( 10 - 'texto')        //NaN
+console.log( 10 + 10 );           //20                
+console.log( 10 == '10');         //true
+console.log( 10 === '10' );       //false
+console.log( 10 != 10 );          //false
+console.log( 10 !== '10');        //true
+
+*/
+
--- variaveis.js
+++ variaveis.js
@@ -1,21 +1,64 @@
 
+/*
 // Exitem 3 tipo de variaveis  var , let e const .
 
 
 
-var  var1 = 1;   
-var  var2 = "teste";
-console.log (var1 + var2);
+var var1 = 1;
+var var2 = "teste";
+console.log(var1 + var2);
 //resultado = 1teste 
 
 const var3 = 3;
 const var4 = 4;
-console.log(var3+var4);
+console.log(var3 + var4);
 //resultado = 7
 
 let var5 = 5
 let var6 = 6
-console.log(var5+var6);
+console.log(var5 + var6);
 // resultado = 11
 
 
+//  var 
+// deixa vaza espoco ;
+if (true) {
+    var var1 = 10;
+}
+console.log(var1);
+
+// não executa a função 
+
+function teste() {
+    var var1 = 15;
+}
+
+teste();
+
+console.log(var1);
+
+// let
+// não executa pq se restringe ao tipo bloco seja função 
+/*if (true){
+    let var1 = 20;
+}
+console.log(var1)
+
+
+// const não muda a varivel  
+
+var y = 10;
+y = 15 
+console.log(y)
+
+// resultado = 15 
+
+// No const
+
+const x = 10;
+//x = 15 ;
+console.log(x)
+// resultado = erro 
+
+
+*/
